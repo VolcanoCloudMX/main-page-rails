0c13cba8e0803b7edd857725de02cb85a8d45d3c
Diego Alejandro Gentner Polanco <73558208+PudinDeveloper@users.noreply.github.com>
2023-08-05 18:16:27 -0600

    Page design

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   get 'code_of_conduct/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#index'
+
   get 'conducta', to: 'code_of_conduct#show'
   get 'reglamento', to: 'code_of_conduct#show'
+
+  get 'faq', to: 'pages#faq'
 end
